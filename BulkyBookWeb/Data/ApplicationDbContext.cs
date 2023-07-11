2cbf926db60456dec77d8c5be4aaf3100a62b948
Karani-Ken <Karani-Ken@KARANI-KEN>
2023-07-11 16:15:23 +0300

    created the application db Context and configured th program.cs file to use the db using the entity framework core sql server

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //configuring the application db context
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {                
+        }
+        //creating a category table in the database using a db set (DbSet)
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
